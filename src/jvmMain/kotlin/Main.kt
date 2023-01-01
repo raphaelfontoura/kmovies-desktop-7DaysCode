c5f5fb3921c66ab00c29a59e08c6e3fe011e26ab
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-01 12:27:14 -0300

    Change image to url and create function to load image by url

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -4,12 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.skia.Image as ImageSkia
+import java.net.URL
+
 
 @Composable
 @Preview
@@ -19,7 +24,7 @@ fun App() {
         titulo = "Batman: O Cavaleiro das Trevas",
         nota = 9.0,
         ano = 2008,
-        imagem = "batman-cavaleiro-trevas.png"
+        imagem = "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/86/98/32/19870786.jpg"
     )
 
     with(batmanMovie) {
@@ -27,13 +32,21 @@ fun App() {
             Column {
                 Text(titulo, fontStyle = FontStyle.Italic, fontWeight = FontWeight.Bold, fontSize = 24.sp)
                 Image(
-                    painter = painterResource(imagem),
+//                    painter = painterResource(imagem),
+                    bitmap = imagem.loadImageBitmap(),
                     contentDescription = "capa $imagem"
                 )
                 Text("Nota: $nota - Ano: $ano")
             }
         }
     }
+}
+
+fun String.loadImageBitmap(): ImageBitmap {
+    val url = URL(this)
+    val openStream = url.openStream()
+    val bytes = openStream.readAllBytes()
+    return ImageSkia.makeFromEncoded(bytes).toComposeImageBitmap()
 }
 
 data class Movie(
