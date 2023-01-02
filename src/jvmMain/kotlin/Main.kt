7f6175637ba8311c884904fa19ba964124fc6679
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-02 14:18:39 -0300

    Create new view

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,17 +1,24 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.darkColors
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import extensions.loadImageBitmapV2
 import model.Movie
+import java.net.CookieHandler
 
 
 @Composable
@@ -27,14 +34,59 @@ fun App() {
 
     with(batmanMovie) {
         MaterialTheme(colors = darkColors()) {
-            Column {
-                Text(titulo, fontStyle = FontStyle.Italic, fontWeight = FontWeight.Bold, fontSize = 24.sp)
-                Image(
-                    bitmap = imagemUrl.loadImageBitmapV2(),
-                    contentDescription = "capa $imagemUrl"
-                )
-                Text("Nota: $nota - Ano: $ano")
+            Surface(
+                modifier = Modifier.widthIn(min = 260.dp, max = 360.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier.padding(10.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Image(
+                            bitmap = imagemUrl.loadImageBitmapV2(),
+                            contentDescription = "capa $imagemUrl"
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Column {
+                                Row {
+                                    Icon(
+                                        Icons.Rounded.Star,
+                                        contentDescription = "star icon",
+                                        tint = Color.Yellow
+                                    )
+                                    Text("$nota")
+                                }
+                            }
+                            Column {
+                                Text("$ano")
+                            }
+                        }
+                        Row(
+                            modifier = Modifier.fillMaxWidth(fraction = 0.8f),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                titulo,
+                                fontStyle = FontStyle.Italic,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 18.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
             }
+
         }
     }
 }
